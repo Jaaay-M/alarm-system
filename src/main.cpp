3f83c8668b1001e65b806f9aab4fd34a45036c28
Jay <jaymanning123@gmail.com>
2026-08-13 01:47:53 +0100

    Created the basic structure of the web server backend, including the index.html file and necessary modifications to the platformio.ini and main.cpp files. This setup allows for serving a simple web page and handling basic HTTP requests.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+#include <LittleFS.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Replace with your network credentials
+const char* ssid = "BLANK";
+const char* password = "BLANK";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Start Serial communication for debugging
+  Serial.begin(115200);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Initialize LittleFS
+  if (!LittleFS.begin()) {
+    Serial.println("An error has occurred while mounting LittleFS");
+    return;
+  }
+
+  Serial.println("LittleFS mounted successfully");
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+
+  // Print the successful connection message and the IP address
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Start the server
+  server.begin();
+  Serial.println("HTTP server started");
+
+  // Serve the index.html file when accessing the root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
 }
